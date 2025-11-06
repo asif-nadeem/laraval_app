fa74a76d0e6e6266128ed5242a2b873e01fffb08
Asif Nadeem <asif.nadeem4198@gmail.com>
2025-11-06 16:41:34 +0500

    some updates with categories Route

--- routes/web.php
+++ routes/web.php
@@ -32,8 +32,21 @@ Route::middleware(['auth'])->group(function () {
     Route::prefix('user')->group(function () {
         Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-        ///Route::get('/categories/index', [App\Http\Controllers\CategoryController::class, 'index']);//->name('list');
+        // not use resource
+//        Route::get('/categories/index', [App\Http\Controllers\CategoryController::class, 'index'])->name('categories.index');
+//
+//        Route::get('/categories/create', [App\Http\Controllers\CategoryController::class, 'create'])->name('categories.create');
+//        Route::post('/categories/store', [App\Http\Controllers\CategoryController::class, 'store'])->name('categories.store');
+//
+//        Route::get('/categories/edit/{id}', [App\Http\Controllers\CategoryController::class, 'edit'])->name('categories.edit');
+//        Route::put('/categories/update/{id}', [App\Http\Controllers\CategoryController::class, 'update'])->name('categories.update');
+//
+//        Route::delete('/categories/destroy/{id}', [App\Http\Controllers\CategoryController::class, 'destroy'])->name('categories.destroy');
+//
+//
+//        Route::get('/categories/show/{id}', [App\Http\Controllers\CategoryController::class, 'show'])->name('categories.show');
 
+        //use resource
         Route::resource('categories', App\Http\Controllers\CategoryController::class)->name('index','categories.index');
     });
 });
